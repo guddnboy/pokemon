a9eca20bb32c11616b93a908858c3ff12ea22f43
guddnboy <hung3018@naver.com>
2025-05-20 14:43:04 +0900

    refact: 반환된 url로 데이터 가져오도록 함수 수정 및 추가

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,12 +1,24 @@
 import instance from './axios';
 
+const BASE_URL = 'https://pokeapi.co/api/v2/';
+
 export const getAllPokemons = async () => {
   try {
-    const response = await instance.get('pokemon/');
+    const response = await instance.get(`${BASE_URL}/pokemon/`);
     return response.data;
   } catch (error) {
     // [TODO] 에러 핸들링 작업 필요
     console.error(error);
     throw error;
+  }
+};
+
+export const getUrlData = async (url: string) => {
+  try {
+    const response = await instance.get(url);
+    return response.data;
+  } catch (error) {
+    // [TODO] 에러 핸들링 작업 필요
+    console.error(error);
   }
 };
--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,9 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://pokeapi.co/api/v2/';
-
 const instance = axios.create({
-  baseURL: BASE_URL,
   method: 'get',
   headers: { 'X-Custom-Header': 'foobar' },
 });
