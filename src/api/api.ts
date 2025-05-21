91091bc71d1d2ecadf3fb9f437336a09bc1c2a17
guddnboy <hung3018@naver.com>
2025-05-21 21:34:21 +0900

    feat: 포켓몬 상세 정보에 한국어 이름, 속성 및 기술 추가

--- src/api/api.ts
+++ src/api/api.ts
@@ -5,7 +5,15 @@ const BASE_URL = 'https://pokeapi.co/api/v2/';
 export const getPokemonDetails = async (id: number) => {
   try {
     const response = await instance.get(`${BASE_URL}/pokemon/${id}`);
-    return response.data;
+    const koreanName = await getKoreanName(id);
+    const koreanGenus = await getKoreanGenus(id);
+    const koreanMove = await getKoreanMove(id);
+    return {
+      ...response.data,
+      name: koreanName,
+      genus: koreanGenus,
+      moves: koreanMove,
+    };
   } catch (error) {
     // [TODO] 에러 핸들링 작업 필요
     console.error(error);
@@ -21,4 +29,41 @@ export const getUrlData = async (url: string) => {
     // [TODO] 에러 핸들링 작업 필요
     console.error(error);
   }
+};
+
+const getKoreanName = async (id: number) => {
+  try {
+    const response = await instance.get(`${BASE_URL}/pokemon-species/${id}`);
+    const koreanData = response.data.names.find(
+      (name: { language: { name: string } }) => name.language.name === 'ko'
+    );
+    return koreanData.name;
+  } catch (error) {
+    // [TODO] 에러 핸들링 작업 필요
+    console.error(error);
+  }
+};
+
+const getKoreanGenus = async (id: number) => {
+  try {
+    const response = await instance.get(`${BASE_URL}/pokemon-species/${id}`);
+    const koreanGenus = response.data.genera.find(
+      (genus: { language: { name: string } }) => genus.language.name === 'ko'
+    );
+    return koreanGenus.genus;
+  } catch (error) {
+    // [TODO] 에러 핸들링 작업 필요
+    console.error(error);
+  }
+};
+
+const getKoreanMove = async (id: number) => {
+  const response = await instance.get(`${BASE_URL}/move/${id}`);
+  const moves = response.data;
+  const koreanMoves = moves.names.find(
+    (name: { language: { name: string } }) => name.language.name === 'ko'
+  );
+  return {
+    name: koreanMoves.name,
+  };
 };
