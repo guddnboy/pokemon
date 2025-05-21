105cec52f091f385537225277472b9b6a2027289
guddnboy <hung3018@naver.com>
2025-05-22 00:05:05 +0900

    feat: 포켓몬 카드 컴포넌트의 데이터 구조 개선 및 타입 태그 스타일 수정

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react';
 import { getPokemonDetails } from '../api/api';
-import { AbilitiesTag } from './tags/AbilitiesTag';
 import { TypeTag } from './tags/TypeTag';
 interface PokemonCardProps {
   id: number;
@@ -12,7 +11,6 @@ export const PokemonCard = ({ id }: PokemonCardProps) => {
   useEffect(() => {
     const fetchData = async () => {
       const data = await getPokemonDetails(id);
-      console.log(data);
       if (data) {
         setPokemon({
           ...data,
@@ -20,9 +18,6 @@ export const PokemonCard = ({ id }: PokemonCardProps) => {
             ability: { name },
           })),
           sprites: { front_default: data.sprites },
-          moves: {
-            name: Array.isArray(data.moves) ? data.moves[0] : data.moves,
-          },
         });
       }
     };
@@ -30,13 +25,13 @@ export const PokemonCard = ({ id }: PokemonCardProps) => {
   }, [id]);
   return (
     <div
-      className={`m-4 p-4 rounded-md shadow-lg ${pokemon?.colorClass} border-2 border-gray-300`}
+      className={`size-60 m-4 p-4 rounded-md shadow-lg bg-${pokemon?.colorClass} border-2 border-gray-300`}
     >
-      <div className="flex justify-between">
-        <div className="text-primary-dark-red font-bolf text-2xl">
-          {pokemon?.name}
-        </div>
-        <div className="bg-primary-dark-blue text-primary-bright-yellow border-2 border-gray-300 rounded-md px-2">
+      <div className="flex justify-between mb-2">
+        <TypeTag typeName={pokemon?.genus} />
+        <div
+          className={`size-6 flex justify-center items-center bg-gray-200 text-${pokemon?.color} border-gray-300 rounded-md shadow-md text-xs font-bold`}
+        >
           {pokemon?.order}
         </div>
       </div>
@@ -47,25 +42,24 @@ export const PokemonCard = ({ id }: PokemonCardProps) => {
           className="bg-transparent border-2 border-gray-300 rounded-xl p-2 shadow-md"
         />
       </div>
-      <div>
-        <TypeTag typeName={pokemon?.genus} />
-      </div>
 
+      <div
+        className={`flex justify-center text-${pokemon?.color} font-bold text-lg`}
+      >
+        <div className="font-bold text-md">{pokemon?.name}</div>
+      </div>
       <div className="flex justify-between">
-        <div>키 : {pokemon?.height}</div>
-        <div>몸무게 : {pokemon?.weight}</div>
+        {/* <div className="text-sm">height : {pokemon?.height}</div>
+        <div className="text-sm">weight : {pokemon?.weight}</div> */}
       </div>
-      <div className="flex justify-center">{pokemon?.moves.name}</div>
-      <div className="flex overflow-x-auto text-xs">
-        {pokemon?.abilities.map((ability, index) => (
-          <AbilitiesTag key={index} abilitiesName={ability.ability.name} />
-        ))}
-      </div>
-      <div className="mt-4">
+      <div className="mt-2">
         <details className="group">
           <summary className="cursor-pointer text-primary-dark-blue hover:text-primary-dark-yellow transition-colors">
             포켓몬 설명 더보기
           </summary>
+          <div className="mt-2 text-sm text-gray-700">
+            {pokemon?.flavorText ? pokemon.flavorText : '설명이 없습니다.'}
+          </div>
         </details>
       </div>
     </div>
--- src/components/tags/TypeTag.tsx
+++ src/components/tags/TypeTag.tsx
@@ -1,7 +1,13 @@
 interface TypeTagProps {
-  typeName: string;
+  typeName: string | undefined;
 }
 
 export const TypeTag = ({ typeName }: TypeTagProps) => {
-  return <div className="bg-gray-200 rounded-md px-2">{typeName}</div>;
+  return (
+    <div
+      className={`h-6 flex justify-end items-center px-2 bg-gray-200 border-gray-300 rounded-md shadow-md text-xs font-bold`}
+    >
+      {typeName}
+    </div>
+  );
 };
