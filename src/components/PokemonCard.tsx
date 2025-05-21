22f132586915f3deee3226dbc96b410c1e2567bc
guddnboy <hung3018@naver.com>
2025-05-21 23:20:20 +0900

    feat: 포켓몬 카드 컴포넌트의 데이터 구조 개선 및 레이아웃 수정

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -6,53 +6,56 @@ interface PokemonCardProps {
   id: number;
 }
 
-interface Pokemon {
-  sprites: {
-    front_default: string;
-  };
-  name: string;
-  abilities: { ability: { name: string } }[];
-  order: number;
-  height: number;
-  weight: number;
-  types: { type: { name: string } }[];
-  genus: string;
-  moves: { name: string };
-}
-
 export const PokemonCard = ({ id }: PokemonCardProps) => {
-  const [pokemon, setPokemon] = useState<Pokemon>();
+  const [pokemon, setPokemon] = useState<Pokemon | null>(null);
 
   useEffect(() => {
     const fetchData = async () => {
       const data = await getPokemonDetails(id);
-      setPokemon(data);
       console.log(data);
+      if (data) {
+        setPokemon({
+          ...data,
+          abilities: data.abilities.map((name: string) => ({
+            ability: { name },
+          })),
+          sprites: { front_default: data.sprites },
+          moves: {
+            name: Array.isArray(data.moves) ? data.moves[0] : data.moves,
+          },
+        });
+      }
     };
     fetchData();
   }, [id]);
   return (
-    <div className="m-4 p-4 bg-gray-200 rounded-md">
+    <div
+      className={`m-4 p-4 rounded-md shadow-lg ${pokemon?.colorClass} border-2 border-gray-300`}
+    >
       <div className="flex justify-between">
-        <div>{pokemon?.name}</div>
-        <div>도감 번호 : {pokemon?.order}</div>
+        <div className="text-primary-dark-red font-bolf text-2xl">
+          {pokemon?.name}
+        </div>
+        <div className="bg-primary-dark-blue text-primary-bright-yellow border-2 border-gray-300 rounded-md px-2">
+          {pokemon?.order}
+        </div>
       </div>
       <div className="flex justify-center">
-        <img src={pokemon?.sprites.front_default} alt={pokemon?.name} />
+        <img
+          src={pokemon?.sprites.front_default}
+          alt={pokemon?.name}
+          className="bg-transparent border-2 border-gray-300 rounded-xl p-2 shadow-md"
+        />
       </div>
-      <div>{pokemon?.genus}</div>
+      <div>
+        <TypeTag typeName={pokemon?.genus} />
+      </div>
 
       <div className="flex justify-between">
         <div>키 : {pokemon?.height}</div>
         <div>몸무게 : {pokemon?.weight}</div>
       </div>
       <div className="flex justify-center">{pokemon?.moves.name}</div>
-      <div className="flex">
-        Types :
-        {pokemon?.types.map((type, index) => (
-          <TypeTag key={index} typeName={type.type.name} />
-        ))}
-      </div>
       <div className="flex overflow-x-auto text-xs">
         {pokemon?.abilities.map((ability, index) => (
           <AbilitiesTag key={index} abilitiesName={ability.ability.name} />
