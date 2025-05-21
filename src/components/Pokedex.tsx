3f7096498cacd89e1c9d9f13bdbe4161ab6a5f91
guddnboy <hung3018@naver.com>
2025-05-22 00:04:50 +0900

    feat: 포켓몬 ID 목록 생성 로직 개선 및 버튼 스타일 수정

--- src/components/Pokedex.tsx
+++ src/components/Pokedex.tsx
@@ -2,25 +2,31 @@ import { useState } from 'react';
 import { PokemonCard } from './PokemonCard';
 export const Pokedex = () => {
   const [pokemonIdList, setPokemonIdList] = useState<number[]>(
-    Array.from({ length: 50 }, () => Math.floor(Math.random() * 100) + 1)
+    Array.from({ length: 20 }, () => Math.floor(Math.random() * 100) + 1)
   );
 
   const generateRandomPokemonId = () => {
     // [FIXME] 무한 스크롤 추가 시 수정 필요
-    const randomId = Math.floor(Math.random() * 100) + 1;
-    setPokemonIdList(prevIds => {
-      if (prevIds.length >= 50) {
-        return prevIds.slice(1).concat(randomId);
-      }
-      return [...prevIds, randomId];
-    });
+    const randomIds = Array.from(
+      { length: 20 },
+      () => Math.floor(Math.random() * 100) + 1
+    );
+    setPokemonIdList(randomIds);
   };
 
   return (
-    <div className="grid grid-cols-1 gap-4 md:grid-cols-3 sm:grid-cols-2 lg:grid-cols-4">
-      <button onClick={generateRandomPokemonId}>Generate Random Pokemon</button>
+    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+      <button
+        className="my-2 col-span-full justify-self-center cursor-pointer bg-primary-dark-blue hover:bg-primary-dark-yellow text-white font-bold py-2 px-4 rounded-md shadow-md transition duration-300 ease-in-out"
+        type="button"
+        onClick={() => generateRandomPokemonId()}
+      >
+        새로운 포켓몬
+      </button>
       {pokemonIdList.map((id, index) => (
-        <PokemonCard key={index} id={id} />
+        <div key={index} className="flex justify-center items-center">
+          <PokemonCard id={id} />
+        </div>
       ))}
     </div>
   );
