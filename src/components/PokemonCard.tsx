f1a36a4234a8f40c067c3454b3a2520cd390e51a
guddnboy <hung3018@naver.com>
2025-05-21 16:28:09 +0900

    feat: PokemonCard 컴포넌트에 id를 기반으로 포켓몬 상세 정보를 가져오는 기능 추가

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,3 +1,33 @@
-export const PokemonCard = () => {
-  return <></>;
+import { useEffect, useState } from 'react';
+import { getPokemonDetails } from '../api/api';
+
+interface PokemonCardProps {
+  id: number;
+}
+
+interface Pokemon {
+  name: string;
+  id: number;
+  height: number;
+  weight: number;
+  types: { type: { name: string } }[];
+}
+export const PokemonCard = ({ id }: PokemonCardProps) => {
+  const [pokemon, setPokemon] = useState<Pokemon>();
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await getPokemonDetails(id);
+      setPokemon(data);
+      console.log(data);
+    };
+    fetchData();
+  }, [id]);
+  return (
+    <div className="m-4 p-4 bg-gray-200 rounded-md">
+      <div>name : {pokemon?.name}</div>
+      <div>id : {pokemon?.id}</div>
+      <div>height : {pokemon?.height}</div>
+      <div>weight : {pokemon?.weight}</div>
+    </div>
+  );
 };
