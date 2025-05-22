beeb287d36fe81056c9eab0058983ae2618c6765
guddnboy <hung3018@naver.com>
2025-05-22 10:08:57 +0900

    refactor: 데이터가 undefined일 경우가 없기 때문에 옵셔널 체이닝 제거

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -25,44 +25,44 @@ export const PokemonCard = ({ id }: PokemonCardProps) => {
   }, [id]);
 
   return (
-    <div
-      className={`size-60 m-4 p-4 rounded-md shadow-lg ${pokemon?.colorClass} border-2 border-gray-300`}
-    >
-      <div className="flex justify-between mb-2">
-        {pokemon && <GenusTag genus={pokemon.genus} />}
+    <div className="card-container">
+      {pokemon && (
         <div
-          className={`size-6 flex justify-center items-center bg-gray-200 border-gray-300 rounded-md shadow-md text-xs font-bold`}
+          className={`size-60 m-4 p-4 rounded-md shadow-lg ${pokemon.colorClass} border-2 border-gray-300`}
         >
-          {pokemon?.order}
-        </div>
-      </div>
-      <div className="flex justify-center">
-        <img
-          src={pokemon?.sprites.front_default}
-          alt={pokemon?.name}
-          className="bg-transparent border-2 border-gray-300 rounded-xl p-2 shadow-md"
-        />
-      </div>
-
-      <div
-        className={`flex justify-center text-${pokemon?.color} font-bold text-lg`}
-      >
-        <div className="font-bold text-md">{pokemon?.name}</div>
-      </div>
-      <div className="flex justify-between">
-        {/* <div className="text-sm">height : {pokemon?.height}</div>
-        <div className="text-sm">weight : {pokemon?.weight}</div> */}
-      </div>
-      <div className="mt-2">
-        <details className="group">
-          <summary className="cursor-pointer text-primary-dark-blue hover:text-primary-dark-yellow transition-colors">
-            포켓몬 설명 더보기
-          </summary>
-          <div className="mt-2 text-sm text-gray-700">
-            {pokemon?.flavorText ? pokemon.flavorText : '설명이 없습니다.'}
+          <div className="flex justify-between mb-2">
+            <GenusTag genus={pokemon.genus} />
+            <div
+              className={`size-6 flex justify-center items-center bg-gray-200 border-gray-300 rounded-md shadow-md text-xs font-bold`}
+            >
+              {pokemon.order}
+            </div>
           </div>
-        </details>
-      </div>
+          <div className="flex justify-center">
+            <img
+              src={pokemon.sprites.front_default}
+              alt={pokemon.name}
+              className="bg-transparent border-2 border-gray-300 rounded-xl p-2 shadow-md"
+            />
+          </div>
+
+          <div
+            className={`flex justify-center text-${pokemon.color} font-bold text-lg`}
+          >
+            <div className="font-bold text-md">{pokemon.name}</div>
+          </div>
+          <div className="mt-2">
+            <details className="group">
+              <summary className="cursor-pointer text-primary-dark-blue hover:text-primary-dark-yellow transition-colors">
+                포켓몬 설명 더보기
+              </summary>
+              <div className="mt-2 text-sm text-gray-700">
+                {pokemon.flavorText}
+              </div>
+            </details>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
