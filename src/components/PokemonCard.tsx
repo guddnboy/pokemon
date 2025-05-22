892b993e84024fec537c20f8db7915dde744002e
guddnboy <hung3018@naver.com>
2025-05-22 09:56:37 +0900

    feat: TypeTag를 GenusTag로 이름 변경

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { getPokemonDetails } from '../api/api';
-import { TypeTag } from './tags/TypeTag';
+import { GenusTag } from './tags/GenusTag';
 interface PokemonCardProps {
   id: number;
 }
@@ -29,7 +29,7 @@ export const PokemonCard = ({ id }: PokemonCardProps) => {
       className={`size-60 m-4 p-4 rounded-md shadow-lg ${pokemon?.colorClass} border-2 border-gray-300`}
     >
       <div className="flex justify-between mb-2">
-        <TypeTag typeName={pokemon?.genus} />
+        {pokemon && <GenusTag genus={pokemon.genus} />}
         <div
           className={`size-6 flex justify-center items-center bg-gray-200 border-gray-300 rounded-md shadow-md text-xs font-bold`}
         >
--- src/components/tags/GenusTag.tsx
+++ src/components/tags/GenusTag.tsx
@@ -1,13 +1,13 @@
-interface TypeTagProps {
-  typeName: string | undefined;
+interface GenusTagProps {
+  genus: string;
 }
 
-export const TypeTag = ({ typeName }: TypeTagProps) => {
+export const GenusTag = ({ genus }: GenusTagProps) => {
   return (
     <div
       className={`h-6 flex justify-end items-center px-2 bg-gray-200 border-gray-300 rounded-md shadow-md text-xs font-bold`}
     >
-      {typeName}
+      {genus}
     </div>
   );
 };
