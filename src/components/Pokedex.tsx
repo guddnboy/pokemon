d0ba9fafbc642b81f961b6b6902425eacb81a34d
guddnboy <hung3018@naver.com>
2025-05-27 16:10:24 +0900

    feat: 의존성 배열에 isLoading 추가

--- src/components/Pokedex.tsx
+++ src/components/Pokedex.tsx
@@ -24,7 +24,7 @@ export const Pokedex = () => {
     if (intersecting && !isLoading) {
       loadMorePokemon();
     }
-  }, [intersecting]);
+  }, [intersecting, isLoading]);
 
   return (
     <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
